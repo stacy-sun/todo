b5a95df7bb230db432def11361f00829385a4fc6
Stacy Sun <stacy_sun@staffs-MacBook-Pro.local>
2018-03-10 21:45:23 -0800

    add filter function

--- Components/todo/todo.js
+++ Components/todo/todo.js
@@ -1,84 +1,102 @@
 import React from 'react';
+import './index.css';
 
 export default class TodoApp extends React.Component {
     constructor(){
         super()
         this.state = {
             items:[
-                {id: 0, name:'item 0', isDone:false},
-                {id: 1, name:'item 1', isDone:false},
+                {id: 0, name:'item 0', isDone:true},
+                {id: 1, name:'item 1', isDone:true},
                 {id: 2, name:'item 2', isDone:false}
             ],
-            id:'',
-            name:'',
-            isDone:false,
-            filter:"All"
+            currentFilter:'All'
         }
-        this.addItem=this.addItem.bind(this);
-        this.handleToggle=this.handleToggle.bind(this);
-        this.handleComplete=this.handleComplete.bind(this);
     }
 
-    addItem() {
+    addItem = () => {
         const newItem = {
             id: Date.now(),
-            name:this.input.value,
-            isDone:false
+            name: this.input.value,
+            isDone: false
         }
         const newItems = this.state.items.concat(newItem)
         this.setState({
-            items:newItems
+            items: newItems
         })
         this.input.value = '';
      }
 
-    handleToggle(e){
-        this.setState({
-            isDone:e.target.checked
-        })
+    handleToggle = (id) => {
+        console.log(id);
+        const newItems = [];
+        this.state.items.forEach(item => {
+            if(item.id === id){
+                item.isDone = !item.isDone;
+                console.log(item.isDone)
+            }
+            newItems.push(item);
+        });
+        this.setState({items:newItems})
     }
 
-    handleComplete() {
-        this.setState({
-            filter:"Complete"
-        })
-        return this.state.items.filter(item => item.isDone === true);
+    handleAll = () => {
+        this.setState({currentFilter: "All"})
     }
 
+    handleComplete = () => {
+        this.setState({currentFilter: "Complete"})
+    }
+
+    handleIncomplete = () => {
+        this.setState({currentFilter: "Incomplete"});
+    }
 
     render(){
-        const items = this.state.items;
-        const listItems = items.map((item) =>
-            <li>
-                <input
-                type="checkbox" onClick={this.handleToggle} />
-                <span>{item.name}</span>
-            </li>
+        const listItems = this.state.items.map((item) => {
+                switch(this.state.currentFilter){
+                    case "All":
+                        break;
+                    case "Complete":
+                        if (!item.isDone) {
+                            return null
+                        }
+                        break;
+                    case "Incomplete":
+                        if (item.isDone) {
+                            return null
+                        }
+                        break;
+                }
+                return (
+                    <li className={item.isDone ? "completed": "incompleted"}>
+                        <input
+                            type="checkbox"
+                            checked={item.isDone}
+                            onClick={() => {this.handleToggle(item.id)}}
+                        />
+                        <span>{item.name}</span>
+                    </li>
+                );
+            }
         );
-        // const complete = this.state.items.filter(function (item) {
-        //     return item.isDone;
-        //   });
 
         return (
-        <div>
-            <h1>Todo App</h1>
-            <input type="text" ref={(input) => {this.input = input;}} placeholder="Add an item" />
-            <button type="submit" onClick={this.addItem}>Add</button>
-            <ul>
-                {listItems}
-            </ul>
-            <div>
-                <span onClick={this.handleAll}>All | </span>
-                <span onClick={this.handleIncomplete}>Incomplete | </span>
-                <span onClick={this.handleComplete}>Complete</span>
+            <div id="container">
+                <h1>Todo App</h1>
+                <div className="add-item">
+                    <input type="text" ref={(input) => {this.input = input;}} placeholder="Add an item" />
+                    <button type="submit" onClick={this.addItem}>Add</button>
+                </div>
+                <ul>
+                    {listItems}
+                </ul>
+                <div className="items-status">
+                    <span onClick={this.handleAll}>All | </span>
+                    <span onClick={this.handleIncomplete}>Incomplete | </span>
+                    <span onClick={this.handleComplete}>Complete</span>
+                </div>
             </div>
-            {console.log('isDone:' + this.state.isDone)}
-
-            {/* <div>
-                <h1>Complete</h1>
-                {complete}
-            </div> */}
-        </div>
         );
     }
 }
